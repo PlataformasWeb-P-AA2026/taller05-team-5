6f53a75b3ae0b1a6f258537deb00ff58e58a370a
AnthonyJoelRY <ajromero12@utpl.edu.ec>
2026-05-07 12:58:51 -0500

    cambio finall

--- formato-json/cargar_couchdb.py
+++ formato-json/cargar_couchdb.py
@@ -5,7 +5,7 @@ from credenciales import USUARIO, PASSWORD, HOST, PUERTO
 URL_BASE = f"http://{USUARIO}:{PASSWORD}@{HOST}:{PUERTO}"
 BASE_DATOS = "jugadores"
 
-# 1. Crear la base de datos jugadores
+# 1. Crear base de datos
 respuesta = requests.put(f"{URL_BASE}/{BASE_DATOS}")
 
 if respuesta.status_code == 201:
@@ -15,16 +15,44 @@ elif respuesta.status_code == 412:
 else:
     print("Error creando la base de datos:", respuesta.text)
 
-# 2. Leer el archivo JSON generado
+# 2. Leer JSON
 with open("mundial_2026.json", "r", encoding="utf-8") as archivo:
     datos = json.load(archivo)
 
-# 3. Cargar documentos a CouchDB con _bulk_docs
+# 3. Cargar documentos
 respuesta = requests.post(
     f"{URL_BASE}/{BASE_DATOS}/_bulk_docs",
     headers={"Content-Type": "application/json"},
     json=datos
 )
 
-print("Estado:", respuesta.status_code)
-print(respuesta.json())
+print("Carga de documentos:", respuesta.status_code)
+
+# 4. Crear vistas automáticamente
+design_doc = {
+    "_id": "_design/losjugadores",
+    "views": {
+        "por_club": {
+            "map": "function(doc) { if (doc.club_actual) { emit(doc.club_actual, doc); } }"
+        },
+        "por_goles": {
+            "map": "function(doc) { if (doc.goles) { emit(doc.goles, doc); } }"
+        },
+        "por_partidos": {
+            "map": "function(doc) { if (doc.partidos) { emit(doc.partidos, doc); } }"
+        }
+    },
+    "language": "javascript"
+}
+
+respuesta = requests.put(
+    f"{URL_BASE}/{BASE_DATOS}/_design/losjugadores",
+    headers={"Content-Type": "application/json"},
+    json=design_doc
+)
+
+if respuesta.status_code in [201, 202]:
+    print("Vistas creadas correctamente")
+else:
+    print("Estado creación de vistas:", respuesta.status_code)
+    print(respuesta.text)
